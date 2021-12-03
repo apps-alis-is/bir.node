f0f0209949c8e12cbe99441ff3c54c12e7ba4060
Grimer123 <35968798+Grimer123@users.noreply.github.com>
2021-12-03 21:03:50 +0300

    Update model.ext.lua
    Update Birake to 4.0.1.0

--- src/model.ext.lua
+++ src/model.ext.lua
@@ -1,6 +1,6 @@
 am.app.set_model(
     {
-        DAEMON_URL = "https://github.com/birake/birakecoin/releases/download/v4.0.0.0/BIR-4.0.0.0-Linux.zip",
+        DAEMON_URL = "https://github.com/birake/birakecoin/releases/download/v4.0.1.0/BIR-4.0.1.0-Linux.zip",
         DAEMON_CONFIGURATION = {
             server = (type(am.app.get_configuration("NODE_PRIVKEY") == "string") or am.app.get_configuration("SERVER")) and 1 or nil,
             listen = (type(am.app.get_configuration("NODE_PRIVKEY") == "string") or am.app.get_configuration("SERVER")) and 1 or nil,
